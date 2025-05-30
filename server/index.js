297bb04b0a4c31ededac057b62686536026b67c1
Harada7 <161343642+Harada7@users.noreply.github.com>
2025-05-30 20:36:22 -0300

    Revert "TFLPI - Ajuste Index do server"

--- server/index.js
+++ server/index.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const path = require('path');
+require('dotenv').config();
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const livroRoutes = require('./routes/livrosRoutes');
-
-dotenv.config();
+const livrosRoutes = require('./routes/livrosRoutes.js');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use('/livros', livroRoutes);
-
-// Servir o frontend (React) buildado
+app.use('/livros', livrosRoutes);
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
-// Conexão com MongoDB + Iniciar servidor
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () =>
