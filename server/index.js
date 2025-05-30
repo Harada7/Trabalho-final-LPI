004889e556ea89f71802f31cb3867f2b8ba261e8
harada <hickmhfgomes591@gmail.com>
2025-05-30 20:13:56 -0300

    TFLPI - Ajuste Index do server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const livrosRoutes = require('./routes/livrosRoutes.js');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/livros', livrosRoutes);
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
